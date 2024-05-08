7d292366e01bcb251e7ec9227444c1dde1bf9147
CodeIsLife <pbsemufali21@gmail.com>
2024-05-08 13:33:13 +0300

    Landing Page

---
+++ src/components/footer.vue
@@ -0,0 +1,178 @@
+<script setup>
+import { RouterLink } from 'vue-router';
+</script>
+<template>
+  <div class="grid grid-cols-12">
+    <div class="col-span-1 bg-alphachain"></div>
+    
+    <div class="main-main-container flex flex-col col-span-10">
+    <div class="flex-shrink-0 opacity-87 bg-alphachain flex justify-between px-10 py-10 items-start">
+      <!-- This holds the first part of the footer -->
+      <div class="flex flex-col items-start c-first-container">
+        <!-- div to hold the alphachain and them icons -->
+        <p class="text-white  font-ubuntu text-4xl font-bold w-85 h-[71.304px] flex-shrink-0">Alphachain</p>
+        <p class="c-follow-us">Follow us</p>
+        <!-- div for them icons -->
+        <div class="pt-4">
+          <ul class="c-links flex ">
+            <li class="pr-2">
+              <a href=""><font-awesome-icon :icon="['fab', 'facebook-f']" class="text-white c-exact-icon p-3 bg-opacity-50 bg-alphachain"
+                  size="1x" /></a>
+            </li>
+            <li class="pr-2">
+              <a href=""><font-awesome-icon :icon="['fab', 'x-twitter']" class="text-white c-exact-icon p-3 bg-opacity-50 bg-alphachain"
+                  size="1x" /></a>
+            </li>
+            <li class="pr-2">
+              <a href=""><font-awesome-icon :icon="['fab', 'linkedin-in']" class="text-white c-exact-icon p-3 bg-opacity-50 bg-alphachain"
+                  size="1x" /></a>
+            </li>
+
+            <li>
+              <a href="https://www.instagram.com/ega_tanzania/"><font-awesome-icon :icon="['fab', 'instagram']" class="text-white c-exact-icon p-3 bg-opacity-50 bg-alphachain"
+                  size="1x" /></a>
+            </li>
+          </ul>
+        </div>
+
+      </div>
+      <!-- this holds the list you feel me -->
+      <div class="c-links ">
+        <ul>
+          <label><span class="c-home-active">Other links</span></label>
+          <li class="mt-5"><a href="https://faucet.all.co.tz/"><span>>Faucet</span></a></li>
+          <li class="mt-5"><a href="https://alphachain.all.co.tz/Wallet"><span>>Wallet</span></a></li>
+          <li class="mt-5"><a href="/"><span>>Explorer</span></a></li>
+          <li class="mt-5 "><a href="https://alphachain.all.co.tz/dapps"><span>>dApps</span></a></li>
+        </ul>
+      </div>
+
+      <!-- this one holds the third part -->
+      <div class="flex flex-col items-start pr-11">
+        <p class="c-join-our-community">Join our community</p>
+        <div class="flex">
+          <button class="
+              bg-btnAmber
+              px-5
+              py-2
+              rounded-md
+              drop-shadow-md
+              text-black
+              mt-3">Join Discord</button>
+          <button class="
+           bg-btnAmber
+              px-5
+              py-2
+              rounded-md
+              drop-shadow-md
+              text-black
+              mt-3
+              ml-2
+              flex
+              items-center
+              "><img src="../assets/alpha-icon.svg" alt="alpha-icon">
+            Add Alphachain Network</button>
+
+        </div>
+      </div>
+    </div>
+    <div class="text-[#D8D8D8] text-center  pb-4 italic bg-alphachain self-center w-full">
+      Developed and maintained by eGA-RIDC, Copyright © 2023 eGA-RIDC
+    </div>
+  </div>
+
+    <div class="col-span-1 bg-alphachain"></div>
+  </div>
+  
+</template>
+<style scoped>
+.c-follow-us {
+  color: #FFF;
+  font-family: Ubuntu;
+  font-size: 20px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+  width: 137px;
+  height: 35.652px;
+  flex-shrink: 0;
+}
+
+.c-facebook {
+  width: 90px;
+  height: 86.721px;
+  flex-shrink: 0;
+}
+
+.c-x-twitter {
+  width: 90px;
+  height: 86.721px;
+  flex-shrink: 0;
+}
+
+.c-linked-in {
+  width: 90px;
+  height: 86.721px;
+  flex-shrink: 0;
+}
+
+.c-instagram {
+  width: 90px;
+  height: 86.721px;
+  flex-shrink: 0;
+}
+
+span {
+  color: white;
+  width: 48px;
+  height: 46.251px;
+  flex-shrink: 0;
+}
+
+.c-links a {
+  color: #FFF;
+  text-align: center;
+  font-family: Ubuntu;
+  font-size: 12px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+}
+.c-links a :hover {
+    color: #FFA140;
+}
+.c-join-our-community {
+  color: #FFF;
+  text-align: center;
+  font-family: Ubuntu;
+  font-size: 30px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: normal;
+}
+
+/* .c-join-button {
+  border-radius: 12px;
+  background: #FFA140;
+  width: 240px;
+  height: 58.778px;
+  flex-shrink: 0;
+} */
+
+.c-exact-icon {
+  font-weight: 4000;
+  /* background-color: white; */
+  border-radius: 9999px;
+  width: 20px;
+  height: 20px;
+}
+
+.c-home-active {
+  font-weight: bold;
+}
+
+
+.c-links ul li a span {
+  font-size: 16px;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,95 +1,127 @@
 <template>
-  <div class="h-auto xl:w-[70%] max-xl:w-full flex flex-col transition-width duration-300 ease-in-out mx-auto">
-    <!-- bg-gradient-to-br from-[#40128B] via-[#401290] to-[#40129F] -->
-    <nav class=" h-[1%] min-h-16 flex flex-row justify-between px-4">
-        <div class="my-auto">
-            <div class="text-4xl font-mono">LOGO</div>
-        </div>
-        <div class="flex flex-row">
-            <div>
-
-            </div>
-
-            <button class="border border-black h-fit p-2 rounded  my-auto hover:text-white font-sans font-medium hover:bg-[#40128B] transition-colors duration-300 ease-in-out">Connect</button>
-        </div>
-    </nav>
-    <div class="h-[99.5vh] bg-gray-100 relative rounded-t-xl overflow-hidden">
-        <div class="h-3/4 w-full relative flex flex-row overflow-hidden">
-            <div class="w-1/2 flex flex-col font-sans px-4 justify-center">
-                <div class="flex flex-col xl:text-5xl md:text-3xl">
-                    <span class="font-bold">Connect&nbsp;all&nbsp;around&nbsp;the&nbsp;world</span>
-                    <span class="gradient-text font-bold">with Demedia</span>
-                </div>
-               <div class="text-xl mt-8">
-                    <span>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magni vitae suscipit ducimus inventore hic. Illo provident voluptas minus harum neque esse enim rerum eius laborum, illum cupiditate perferendis veritatis dolorem.</span>
-               </div>
-            </div>
-
-            <div class="ml-auto mr-0 w-1/2 h-full flex bg-cover object-fit-cover bg-size-contain">
-                <img class="ml-auto" src="/src/assets/images/landing/pattern.jpeg" alt="">
-            </div>
-
-            <div
-            class="top-auto bottom-0 left-0 right-0 w-full absolute pointer-events-none overflow-hidden"
-            style="height: 70px;"
-            >
-            <svg
-                class="absolute bottom-0 overflow-hidden"
-                xmlns="http://www.w3.org/2000/svg"
-                preserveAspectRatio="none"
-                version="1.1"
-                viewBox="0 0 2560 100"
-                x="0"
-                y="0"
-            >
-                <polygon
-                class="text-gray-300 fill-current"
-                points="2560 0 2560 100 0 100"
-                ></polygon>
-            </svg>
-            </div>
-        </div>
-
-        <div class="h-1/4 bg-gray-300">
+    <div class="w-full">
+        <div class="w-full flex flex-col items-center transition-width duration-300 ease-in-out mx-auto ">
+            <!-- bg-gradient-to-br from-[#40128B] via-[#401290] to-[#40129F] -->
             
-        </div>
-
-         
-    </div>
-    <div class="h-screen relative">
-        <div class="h-1/2 relative bg-gray-300">
-            <div class="flex flex-col bg-gray-300 h-[100%] px-8">
-                <div class="flex font-sans text-2xl text-gray-600 font-medium items-center justify-center text-center">
-                    Demedia is a lorem ipsum, dolor asdaf dsaf asdf <br>sit amet consectetur adipisicing afaf adfgdf  adfasdfgf
+            <div class="h-[100vh] w-full max-xl:bg-gradient-to-br flex flex-col text-gray-200 relative overflow-hidden">
+                <div class="absolute top-0 left-0 h-[100%] w-[100%] flex bg-cover object-fit-cover bg-size-contain ">
+                    <img width="100%" src="/src/assets/images/landing/patternpad2.svg" alt="">
                 </div>
+                <nav class="w-full z-10 h-[1%] min-h-16 flex ">
+                    <div class="m-auto w-[70%] flex flex-row justify-between px-4 items-center">
+                        <div class="my-auto">
+                            <div class="text-4xl font-mono text-white w-24"><img width="100%" src="/src/assets/images/eGAlogo.png" alt=""></div>
+                        </div>
+                        <div class="flex flex-row">
+                            <div>
 
-                <div class="flex flex-row space-x-8 mt-8 w-full h-[80%] relative justify-center py-1 transition-all duration-600 ease-in">
-                    <div class="w-1/3 h-[100%] overflow-hidden rounded-[1.1rem] bg-gray-400 hover:bg-gradient-to-r hover:from-[#40128B] hover:via-[#a46fff] hover:to-[#5804ff] p-[2px]">
-                        <div class="bg-gray-300 h-[100%] rounded-[0.9rem] flex flex-row">
-                            
+                            </div>
+
+                            <button @click="connect()" class="border border-black h-fit px-4 py-2 text-xl rounded  my-auto text-white font-sans font-medium hover:bg-[#7e2eff8c] bg-[#7e2eff] transition-colors duration-150 ease-in-out">Connect</button>
                         </div>
                     </div>
-                    
-                    <div class="w-1/3 h-[100%] overflow-hidden rounded-[1.1rem] bg-gray-400 hover:bg-gradient-to-r hover:from-[#40128B] hover:via-[#a46fff] hover:to-[#5804ff] p-[2px]">
-                        <div class="bg-gray-300 h-[100%] rounded-[0.9rem] flex flex-row">
-                            
+                </nav>
+                <div class="h-3/4 w-[70%] mx-auto relative flex flex-row overflow-hidden ">
+                    <div class="w-[50%] flex flex-col font-sans px-4 justify-center ">
+                        <div class="flex flex-col xl:text-5xl md:text-3xl">
+                            <span class="font-bold bg-gradient-to-br from-[#40128bb9] via-[#401290b9] to-[#41129f9f] rounded">Connect&nbsp;all&nbsp;around&nbsp;the&nbsp;world</span>
+                            <div class="bg-gray-60 bg-opacity-25 rounded-b"> <span class="gradient-text font-bold">with AlphaConnect.</span></div>
+                           
+                        </div>
+                        <div class="text-lg mt-4 text-gray-200 font-semibold">
+                            <span>Share and Connect</span>
                         </div>
                     </div>
+
+                    <!-- <div class="ml-auto mr-0 w-1/2 h-full flex bg-cover object-fit-cover bg-size-contain">
+                        <img class="ml-auto" src="/src/assets/images/landing/pattern.jpeg" alt="">
+                    </div> -->
+                </div>
+               
+            </div>
+            <div class="h-screen w-full relative">
+                <div class="h-[60%] relative ">
+                    <div class="flex flex-col bg-gray-100 h-[100%] px-8 justify-center items-center space-y-6 max-md:space-y-3">
+                        <div class="flex flex-col space-y-6 max-md:space-y-3">
+                            <div class="text-4xl font-bold mx-auto text-[#40128b]">
+                                Why AlphaConnect?
+                            </div>
+                            <div class="text-lg font-semibold text-gray-400 text-center">
+                                Alphaconnect provides a decentralized platform <br>for sharing media content and connecting with people of all walks of life,<br> wherever in the world. 
+                            </div>
+                        </div>
+                        <div class="flex flex-row space-x-4 font-sans">
+                            <div class="flex flex-col items-center justify-center w-64">
+                                <div class="w-20 h-20 flex justify-center items-center rounded-full bg-gray-200">
+                                    
+                                </div>
+                                <div class="text-2xl font-[600] mx-auto text-[#40128b]">
+                                    Share
+                                </div>
+                                <div class="text-md font-semibold text-gray-400 text-center">
+                                        Post and share pictures with friends <br> <br>
+                                </div>
+                            </div>
+                            <div class="flex flex-col items-center justify-center w-64">
+                                <div class="w-20 h-20 flex justify-center items-center rounded-full bg-gray-200">
+                                    
+                                </div>
+                                <div class="text-2xl font-[600] mx-auto text-[#40128b]">
+                                    Connect
+                                </div>
+                                <div class="text-md font-semibold text-gray-400 text-center">
+                                    Make new friends and connections while maintaining control of your own data
+                                </div>
+                            </div>
+                            <div class="flex flex-col items-center justify-center w-64 ">
+                                <div class="w-20 h-20 flex justify-center items-center rounded-full bg-gray-200">
+                                    
+                                </div>
+                                <div class="text-2xl font-[600] mx-auto text-[#40128b]">
+                                    Influence
+                                </div>
+                                <div class="text-md font-semibold text-gray-400 text-center">
+                                       hold an account with less of the Web2 social media platform's security vulnerabilities
+                                </div>
+                            </div>
+                        </div>
+
+                        <!-- <div class="flex font-sans text-2xl text-gray-600 font-medium items-center justify-center text-center">
+                            Demedia is a lorem ipsum, dolor asdaf dsaf asdf <br>sit amet consectetur adipisicing afaf adfgdf  adfasdfgf
+                        </div> -->
+
+                        <!-- <div class="flex flex-row space-x-8 mt-8 w-full h-[80%] relative justify-center py-1 transition-all duration-600 ease-in">
+                            <div class="w-1/3 h-[100%] z-20 hover:shadow-lg overflow-hidden rounded-[1.1rem] bg-gray-400 hover:bg-gradient-to-r hover:from-[#40128B] hover:via-[#a46fff] hover:to-[#5804ff] p-[2px]">
+                                <div class="bg-gray-300 h-[100%] rounded-[0.9rem] flex flex-row">
+                                    
+                                </div>
+                            </div>
+                            
+                            <div class="w-1/3 h-[100%] z-20 hover:shadow overflow-hidden rounded-[1.1rem] bg-gray-400 hover:bg-gradient-to-r hover:from-[#40128B] hover:via-[#a46fff] hover:to-[#5804ff] p-[2px]">
+                                <div class="bg-gray-300 h-[100%] rounded-[0.9rem] flex flex-row">
+                                    
+                                </div>
+                            </div>
+                        </div> -->
+                    </div>
+                </div>
+                <div class="bottom-0 h-[40%] relative">
+                    <div class="absolute top-0 left-0 h-[100%] w-[100%] flex bg-cover object-fit-cover bg-size-contain ">
+                        <img width="100%" src="/src/assets/images/landing/patternpad2.svg" alt="">
+                    </div>
+                    <!-- <footerView class="z-20" /> -->
                 </div>
             </div>
         </div>
-         <div class="bottom-0 h-1/2 bg-green-400">
-            footer
-        </div>
     </div>
-  </div>
+
 </template>
 
 <script setup>
 import { useRouter } from 'vue-router';
 import { ref } from 'vue'; // Import ref from Vue
 import {walletAddressConnected} from "@/scripts/ContractConstants";
-
+import footerView from "@/components/footer.vue"
 const router = useRouter();
 
 // Define connect function to navigate to the specified route
